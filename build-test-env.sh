48e6cc1adae16131dd1327f89e9c215d7d15c575
Vinicius Bohrer <vbsanttos@gmail.com>
2022-12-10 22:35:20 +0000

    update script

--- build-test-env.sh
+++ build-test-env.sh
@@ -1,6 +1,21 @@
-#!/bin/bash
+#!/bin/bash -x
 
-echo "BUILDING TEST ENV"
+if [[ -z $(docker images | grep "vbsantos-tcc/frontend") ]];
+then
+    echo "Building Frontend for tests"
+    git clone "https://github.com/vbsantos/fazumbem-frontend-tcc.git"
+    git checkout pipeline-test
+    docker build --rm -t vbsantos-tcc/frontend:latest ./fazumbem-frontend-tcc
+fi
 
+if [[ -z $(docker images | grep "vbsantos-tcc/backend") ]];
+then
+    echo "Building Backend for tests"
+    git clone "https://github.com/vbsantos/fazumbem-backend-tcc.git"
+    git checkout pipeline-test
+    docker build --rm -t vbsantos-tcc/backend:latest ./fazumbem-backend-tcc
+fi
+
+echo "Building ENV"
 docker compose up --build -d
 
