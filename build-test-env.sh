518a9750bfd5f01684a10cc38aee5faa251a0088
Vinicius Bohrer <vbsanttos@gmail.com>
2022-12-10 22:41:43 +0000

    fix script

--- build-test-env.sh
+++ build-test-env.sh
@@ -4,16 +4,20 @@ if [[ -z $(docker images | grep "vbsantos-tcc/frontend") ]];
 then
     echo "Building Frontend for tests"
     git clone "https://github.com/vbsantos/fazumbem-frontend-tcc.git"
+    cd fazumbem-frontend-tcc
     git checkout pipeline-test
     docker build --rm -t vbsantos-tcc/frontend:latest ./fazumbem-frontend-tcc
+    cd ..
 fi
 
 if [[ -z $(docker images | grep "vbsantos-tcc/backend") ]];
 then
     echo "Building Backend for tests"
     git clone "https://github.com/vbsantos/fazumbem-backend-tcc.git"
+    cd fazumbem-backend-tcc
     git checkout pipeline-test
     docker build --rm -t vbsantos-tcc/backend:latest ./fazumbem-backend-tcc
+    cd ..
 fi
 
 echo "Building ENV"
