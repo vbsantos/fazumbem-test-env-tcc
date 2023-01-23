76f17d95101b791d57c4f92b06f360d6dca570ad
Vinícius Bohrer dos Santos <vinicius.bohrer@voalle.com.br>
2023-01-22 22:04:50 -0300

    Ajuste

--- build-test-env.sh
+++ build-test-env.sh
@@ -21,7 +21,7 @@ if [[ -z $(docker images | grep "vbsantos-tcc/backend") ]]; then
 fi
 
 # install cypress required libs
-apt install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb npm -y
+# apt install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb npm -y
 
 echo "Building ENV"
 
