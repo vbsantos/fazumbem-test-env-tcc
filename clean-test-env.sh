136bd59dc5fe5d23e2c19bc79f0a7791361df259
Vinícius Bohrer dos Santos <vinicius.bohrer@voalle.com.br>
2023-01-22 21:44:59 -0300

    Ajuste

---
+++ clean-test-env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -x
+
+echo "Cleaning ENV"
+
+docker-compose down --volumes
--- run-automated-acceptance-tests.sh
+++ run-automated-acceptance-tests.sh
@@ -5,5 +5,3 @@ echo "Running E2E Cypress tests"
 cd acceptance-tests/
 npm install
 npm run start
-
-docker compose down --volumes
